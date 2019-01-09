120d5d92ef56f79bc43f817368ad6c505701f0d7
Stephen <Blaney.stephen9@gmail.com>
2019-01-09 22:57:04 +0000

    Lab 1

---
+++ Lab1Part1/src/EclipseSimpleExample.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @author Stephen Blaney
+ *
+ */
+
+public class EclipseSimpleExample {
+
+	public static void main(String[] args) {
+		String str = "Hello world";
+		printGreeting(str);
+	}
+
+	/**
+	 * 
+	 * @param pringreeting
+	 *            method to print string str tp main method
+	 */
+
+	public static void printGreeting(String str) {
+
+		System.out.print(str);
+
+	}
+
+}
